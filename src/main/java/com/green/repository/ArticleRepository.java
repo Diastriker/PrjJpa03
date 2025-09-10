a11cc123e61adcb47c7609b0bc15907506ff083e
Diastriker <qortmfahr@gmail.com>
2025-09-10 17:13:13 +0900

    내가 만든 json도 포함

---
+++ src/main/java/com/green/repository/ArticleRepository.java
@@ -0,0 +1,23 @@
+package com.green.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.green.dto.Article;
+
+@Repository
+public interface ArticleRepository
+                             // Article 타입의, Key타입이 Long인것
+       extends   CrudRepository<Article, Long>{
+	
+	@Override
+	List<Article> findAll();
+	// Iterable<> findAll()  return -> List<Article> findAll()
+	// 상속관계를 이용하여 List를 Iterable 로 UpCasting 하여 Casting
+	
+	@Transactional
+	void deleteByTitle(String string);
+}
